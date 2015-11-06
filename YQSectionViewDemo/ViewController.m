bc2d7c4c9437f9a4c5256655d5dff7a0e12a9c0f
yuyedaidao <wyqpadding@gmail.com>
2015-11-06 15:49:30 +0800

    1.4.0

--- YQSectionViewDemo/ViewController.m
+++ YQSectionViewDemo/ViewController.m
@@ -17,10 +17,33 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    YQSectionView *section = [[YQSectionView alloc] initWithItemCount:10];
+    YQSectionView *section = [[YQSectionView alloc] initWithItemCount:2 equalWidth:NO];
     [self.view addSubview:section];
     section.separatorInset = UIEdgeInsetsMake(0, 15, 0, 15);
-    section.frame = CGRectMake(0, 0, 200, 500);
+    section.frame = CGRectMake(0, 10, 200, 500);
+    [section manualConfigureItems];
+    {
+        YQItemCell *cell = [section cellAtIndex:0];
+        UIView *redView = [[UIView alloc] init];
+        redView.backgroundColor = [UIColor redColor];
+        [cell addSubview:redView];
+        
+        redView.translatesAutoresizingMaskIntoConstraints = NO;
+        
+        [cell addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[redView]-0-|" options:0 metrics:nil views:@{@"redView":redView}]];
+        [cell addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[redView(44)]-0-|" options:0 metrics:nil views:@{@"redView":redView}]];
+    }
+    {
+        YQItemCell *cell = [section cellAtIndex:1];
+        UIView *redView = [[UIView alloc] init];
+        redView.backgroundColor = [UIColor blueColor];
+        [cell addSubview:redView];
+        
+        redView.translatesAutoresizingMaskIntoConstraints = NO;
+        
+        [cell addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[redView]-0-|" options:0 metrics:nil views:@{@"redView":redView}]];
+        [cell addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[redView]-0-|" options:0 metrics:nil views:@{@"redView":redView}]];
+    }
     
     
 }
